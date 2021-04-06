a6f0cd030bdafc4f966a45f09fffb4456fbb4b6b
Caybo Kotze <kotzecabo@gmail.com>
2021-04-06 23:01:53 +0200

    :sparkles: Add some more parameters/arguments and logic to cmd line tool

--- main.go
+++ main.go
@@ -16,13 +16,22 @@ import (
 func main() {
 	sqlNew := flag.Bool("sql-new", false, "flag that set's whether a new sql migration needs to be created.")
 	sqlUp := flag.Bool("sql-up", false, "flag that is set to define whether existing migrations should be run.")
+	sqlUser := flag.String("sql-user", os.Getenv("SQL_USER"), "the sql user that needs to be used to execute migrations")
+	sqlPassword := flag.String("sql-password", os.Getenv("SQL_PASSWORD"), "the sql user password that is required to execute the migrations")
+	sqlPort := flag.String("sql-port", os.Getenv("SQL_PORT"), "the sql port that is required to open a db connection")
+	sqlHost := flag.String("sql-host", os.Getenv("SQL_HOST"), "the sql host that is required to open a db connection")
+	sqlDatabase := flag.String("sql-database", os.Getenv("SQL_DATABASE"), "the targeted database that is required to open a db connection")
 
 	flag.Parse()
 
-	//sql_user := flag.String("sql_user", "sqltracking", "the sql user that needs to be used to execute migrations")
-	//user := os.Getenv("SQL_USER")
-	//password := os.Getenv("SQL_PASSWORD")
-	//host := os.Getenv("SQL_HOST")
+	if sqlPort == nil {
+		*sqlPort = "3306"
+	}
+
+	if sqlHost == nil {
+		*sqlHost = "127.0.0.1"
+	}
+
 	//database := os.Getenv("SQL_DATABASE")
 	//dryRun := os.Getenv("DRY_RUN")
 	//mode := os.Getenv("MODE")
@@ -36,15 +45,20 @@ func main() {
 	}
 	if *sqlNew == true && *sqlUp == true {
 		color.Cyan.Println("You can not run sql-new and sql-up at the same time, only sql-new will be run...")
-		os.Exit(1)
+		createNewMigration()
 	}
 	if *sqlNew == true {
 		createNewMigration()
-		os.Exit(1)
+		os.Exit(0)
 	}
 	if *sqlUp == true {
+		if *sqlUser == "" || *sqlPassword == "" || *sqlDatabase == "" {
+			color.Red.Println("You are required to provide a sql user, password and database name, either as a argument or environment variable")
+			fmt.Println(`e.g: mysql-migrator -sql-up -sql-database="doggy_db" -sql-user="doggo" -sql-password="le-woof"`)
+			os.Exit(1)
+		}
 		runMigrations()
-		os.Exit(1)
+		os.Exit(0)
 	}
 }
 
@@ -55,14 +69,18 @@ func createNewMigration() {
 	text, _ := reader.ReadString('\n')
 	re := regexp.MustCompile(`\r?\n`)
 	text = re.ReplaceAllString(text, "")
-	var scriptName string = time.Now().Format("20060102") + "-" + text
-	var upScript string = scriptName + "_up"
-	var downScript string = scriptName + "_down"
+	scriptName := getTimestampAsString() + "-" + text
+	upScript := scriptName + "_up"
+	downScript := scriptName + "_down"
 	err := ioutil.WriteFile("./scripts/" +upScript+ ".sql", []byte(""), 0755)
 	_ = ioutil.WriteFile("./scripts/"+downScript+".sql", []byte(""), 0755)
 	if err != nil {
 		fmt.Printf("Unable to write file: %v\n", err)
 	}
+}
+
+func getTimestampAsString() string {
+	return time.Now().Format("20060102150405")
 }
 
 type Schema struct {

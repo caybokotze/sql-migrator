f8ce1a4a670c0ccb05d873a406eb0e5a9298d07d
Caybo Kotzé <kotzecabo@gmail.com>
2021-06-29 12:25:30 +0200

    :sparkles: create script folder if not exist

--- new-migration.go
+++ new-migration.go
@@ -12,18 +12,19 @@ import (
 func createNewMigration() {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Println("Create a new name for a migration: ")
-	fmt.Println("-> ")
+	fmt.Print("-> ")
 	text, _ := reader.ReadString('\n')
 	re := regexp.MustCompile(`\r?\n`)
 	text = re.ReplaceAllString(text, "")
 	scriptName := getTimestampAsString() + "_" + text
 	upScript := scriptName + "_up"
 	downScript := scriptName + "_down"
+	_ = os.Mkdir("scripts", 0755)
 	err := ioutil.WriteFile(fmt.Sprintf("./scripts/%s.sql", upScript), []byte(""), 0755)
-	_ = ioutil.WriteFile(fmt.Sprintf("./scripts/%s.sql", downScript), []byte(""), 0755)
 	if err != nil {
-		fmt.Printf("Unable to write file: %v\n", err)
+		panic(err.Error())
 	}
+	_ = ioutil.WriteFile(fmt.Sprintf("./scripts/%s.sql", downScript), []byte(""), 0755)
 }
 
 func getTimestampAsString() string {
